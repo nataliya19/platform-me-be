3411def51122e258cc240ca0bfb6bb0e3fbc179f
nataliya.pasichnyk <nataliya.pasichnyk@codility.com>
2020-10-23 18:06:19 +0200

    Update appolo settings

--- index.js
+++ index.js
@@ -31,9 +31,8 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true });
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
